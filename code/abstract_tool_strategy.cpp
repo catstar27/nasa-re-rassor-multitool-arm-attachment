c1008a8d1370c01736d36f2083c24c154a354f96
Elijah Ingerson <90364707+catstar27@users.noreply.github.com>
2026-04-04 12:37:53 -0400

    Remove erroneous print statements

--- code/abstract_tool_strategy.cpp
+++ code/abstract_tool_strategy.cpp
@@ -1,7 +1,6 @@
 #include "abstract_tool_strategy.h"
 #include "execution_data.h"
-#include <cstdio>
 
 void AbstractToolStrategy::execute(ExecutionData execution_data){
-  printf("TODO: Strategy Execution\n");
+  return;
 }
--- code/motion_utilities.cpp
+++ code/motion_utilities.cpp
@@ -1,9 +1,8 @@
 #include "motion_utilities.h"
-#include <cstdio>
 
 Coordinate MotionUtilities::get_position(){
   return current_position;
 }
 void MotionUtilities::move_toward(Coordinate){
-  printf("TODO: Move Toward Coordinate\n");
+  return;
 }
