599949d3480ba19af6e7045accc8351e6cd1eb1a
dKholyusev <dmitry.kholyusev@gmail.com>
2026-04-04 14:48:00 -0400

    Added Inverse Kinematics equations

---
+++ code/degrees.h
@@ -0,0 +1,11 @@
+#ifndef DEGREES_H
+#define DEGREES_H
+
+class Degrees{
+  public:
+    float swivel_degree;
+    float shoulder_degree;
+    float elbow_degree;
+    float wrist_degree;
+};
+#endif
