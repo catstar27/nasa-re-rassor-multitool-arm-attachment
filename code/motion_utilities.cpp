459d5279fced7603aedacc4b37914afe61698841
dKholyusev <dmitry.kholyusev@gmail.com>
2026-04-08 12:39:14 -0400

    Add files via upload

--- code/motion_utilities.cpp
+++ code/motion_utilities.cpp
@@ -14,8 +14,10 @@ void MotionUtilities::move_toward(Coordinate destination){
 Degrees MotionUtilities::inverse_kinematics(Coordinate target_pos){
   Degrees deg = Degrees();
 
-  double distance_squared = target_pos.x*target_pos.x + target_pos.y*target_pos.y;
-  double distance = sqrt(distance_squared); //Finding the distance away from the robot arm.
+  double distance_squared_xz = target_pos.x*target_pos.x + target_pos.z*target_pos.z;
+  double distance_xz = sqrt(distance_squared_xz); //Finding the distance away from the robot arm.
+  double distance_squared_disy = distance_xz*distance_xz + target_pos.y*target_pos.y;
+  double distance_disy = sqrt(distance_squared_disy); //Finding the distance away from the robot arm.
 
   if(distance > shoulder_to_elbow_len + elbow_to_wrist_len){
     Serial.print("Error: coordinate too far");
@@ -24,7 +26,7 @@ Degrees MotionUtilities::inverse_kinematics(Coordinate target_pos){
 
   deg.wrist_degree = 0; //the wrist is at the end point of the arm, it best turn when something else calls upon it.
   deg.swivel_degree = atan(target_pos.z/target_pos.x) * (180.0 / PI); //gets the angle for the swivel in radians, that is then converted to degrees.
-  double angle2 = -acos((distance - shoulder_to_elbow_len*shoulder_to_elbow_len - elbow_to_wrist_len*elbow_to_wrist_len) / (2 * elbow_to_wrist_len * shoulder_to_elbow_len));
+  double angle2 = -acos((distance_squared_disy - shoulder_to_elbow_len*shoulder_to_elbow_len - elbow_to_wrist_len*elbow_to_wrist_len) / (2 * elbow_to_wrist_len * shoulder_to_elbow_len));
   double angle1 = atan((elbow_to_wrist_len * sin(angle2))/(elbow_to_wrist_len*cos(angle2) + shoulder_to_elbow_len));
   //Obtained the angles for the shoulder and elbow via inverse kinematics and elbow-down orientation.
   deg.shoulder_degree = angle1 * (180.0 / PI);
